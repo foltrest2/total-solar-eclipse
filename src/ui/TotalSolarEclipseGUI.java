611e53792671c1944913b38bdee058d87cd086f0
CH1GU1 <kamcak@hotmail.es>
2020-11-26 08:18:12 -0500

    starts and planets working

--- src/ui/TotalSolarEclipseGUI.java
+++ src/ui/TotalSolarEclipseGUI.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import model.Moon;
@@ -24,6 +25,51 @@ public class TotalSolarEclipseGUI {
 	private Stage window;
 
 	private Moon m;
+
+	@FXML
+	private Circle mars;
+
+	@FXML
+	private Circle venus;
+
+	@FXML
+	private Circle jupiter;
+
+	@FXML
+	private Circle star1;
+
+	@FXML
+	private Circle star2;
+
+	@FXML
+	private Circle star3;
+
+	@FXML
+	private Circle star4;
+
+	@FXML
+	private Circle star5;
+
+	@FXML
+	private Circle star6;
+
+	@FXML
+	private Circle star7;
+
+	@FXML
+	private Circle star8;
+
+	@FXML
+	private Circle star9;
+
+	@FXML
+	private Circle star10;
+
+	@FXML
+	private Circle star11;
+
+	@FXML
+	private Circle star12;
 
 	boolean bouncing;
 
@@ -81,7 +127,8 @@ public class TotalSolarEclipseGUI {
 		//primera mitad
 		if((moon.getLayoutX() >= 162 &&  moon.getLayoutX() < 182)|| (moon.getLayoutX() >= 330 &&  moon.getLayoutX() < 341)) {	
 			mainPane.setBackground(new Background(new BackgroundFill(Color.web("#48D1CC"), null, null)));
-		}else if((moon.getLayoutX() >= 182 &&  moon.getLayoutX() < 202)||(moon.getLayoutX() >= 310 &&  moon.getLayoutX() < 330)) {	
+		}
+		else if((moon.getLayoutX() >= 182 &&  moon.getLayoutX() < 202)||(moon.getLayoutX() >= 310 &&  moon.getLayoutX() < 330)) {	
 			mainPane.setBackground(new Background(new BackgroundFill(Color.web("#BEBEBE"), null, null)));
 		}else if((moon.getLayoutX() >= 202 &&  moon.getLayoutX() < 220)||(moon.getLayoutX() >= 290 &&  moon.getLayoutX() < 310)) {	
 			mainPane.setBackground(new Background(new BackgroundFill(Color.web("#191970"), null, null)));
@@ -92,6 +139,42 @@ public class TotalSolarEclipseGUI {
 		}
 		else {
 			mainPane.setBackground(new Background(new BackgroundFill(Color.web("#FFFF00" ), null, null)));
+		}
+		if(moon.getLayoutX() >= 162 &&  moon.getLayoutX() < 351) {
+			mars.setVisible(true);
+			venus.setVisible(true);
+			jupiter.setVisible(true);
+			star1.setVisible(true);
+			star1.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+			star2.setVisible(true);
+			star2.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+			star3.setVisible(true);
+			star3.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+			star4.setVisible(true);
+			star4.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+			star5.setVisible(true);
+			star5.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+			star6.setVisible(true);
+			star6.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+			star7.setVisible(true);
+			star7.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+			star8.setVisible(true);
+			star8.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+			star9.setVisible(true);
+			star9.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+			star10.setVisible(true);
+			star10.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+			star11.setVisible(true);
+			star11.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+			star12.setVisible(true);
+			star12.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+
+
+
+		} else {
+			mars.setVisible(false);
+			venus.setVisible(false);
+			jupiter.setVisible(false);
 		}
 	}
 }
