febc8515a1d0f802950a1831cf0d900c2ecc0d31
John Kennedy Landazuri Sandoval <jkls1998@gmail.com>
2020-11-25 19:55:17 -0500

    moving colors

--- src/ui/TotalSolarEclipseGUI.java
+++ src/ui/TotalSolarEclipseGUI.java
@@ -3,6 +3,10 @@ package ui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class TotalSolarEclipseGUI {
@@ -13,20 +17,52 @@ public class TotalSolarEclipseGUI {
     @FXML
     private AnchorPane moon;
     
+    @FXML
+    private AnchorPane realSun;
+    
+
+    @FXML
+    private Pane mainPane;
+
+    
     public final static double STEP = 25; 
 
     @FXML
     public void moveBackward(ActionEvent event) {
-    	double x = sun.getLayoutX();
+    	double x = moon.getLayoutX();
     	x = x - STEP;
-    	sun.setLayoutX(x);
+    	moon.setLayoutX(x);
+    	changes();
     }
 
-    @FXML
+
+	@FXML
     public void moveForward(ActionEvent event) {
-    	double x = sun.getLayoutX();
+    	double x = moon.getLayoutX();
     	x = x + STEP;
-    	sun.setLayoutX(x);
+    	moon.setLayoutX(x);
+    	changes();
     }
+	
 
+    private void changes() {
+    	//primera mitad
+		if((moon.getLayoutX() >= 162 &&  moon.getLayoutX() < 182)|| (moon.getLayoutX() >= 330 &&  moon.getLayoutX() < 341)) {	
+			mainPane.setBackground(new Background(new BackgroundFill(Color.web("#48D1CC"), null, null)));
+		}else if((moon.getLayoutX() >= 182 &&  moon.getLayoutX() < 202)||(moon.getLayoutX() >= 310 &&  moon.getLayoutX() < 330)) {	
+			mainPane.setBackground(new Background(new BackgroundFill(Color.web("#BEBEBE"), null, null)));
+		}else if((moon.getLayoutX() >= 202 &&  moon.getLayoutX() < 220)||(moon.getLayoutX() >= 290 &&  moon.getLayoutX() < 310)) {	
+			mainPane.setBackground(new Background(new BackgroundFill(Color.web("#191970"), null, null)));
+		}else if((moon.getLayoutX() >= 220 &&  moon.getLayoutX() < 240)||(moon.getLayoutX() >= 270 &&  moon.getLayoutX() < 290)) {	
+			mainPane.setBackground(new Background(new BackgroundFill(Color.web("#000080"), null, null)));
+		}else if(moon.getLayoutX() >= 240 &&  moon.getLayoutX() < 270) {	
+			mainPane.setBackground(new Background(new BackgroundFill(Color.web("#000000"), null, null)));
+		}
+		else {
+			mainPane.setBackground(new Background(new BackgroundFill(Color.web("#FFFF00" ), null, null)));
+		}
+	}
+	
+	
+    
 }
